28d4330d1e1a28508737b318e1d522e733523769
Clifton Ferguson <clifton.ferguson7@gmail.com>
2023-08-06 18:35:06 -0500

    merged with gameframeclass

--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module snakeGame {
+	requires java.desktop;
 }
---
+++ src/snakeGame/GamePanel.java
@@ -0,0 +1,15 @@
+package snakeGame;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
